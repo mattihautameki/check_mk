2b6159a2d2ce877f20437cab61a52a3ad80e0975
Markus Anglmaier <markus@oderdoch.net>
2017-05-26 10:34:32 +0200

    Hosts in Downtime are not part of the stalesvc check anymore.

--- statesvc/agents/stalesvc.py
+++ statesvc/agents/stalesvc.py
@@ -2,9 +2,10 @@
 #
 # Sample program for accessing the Livestatus Module
 # from a python program
+import socket
 socket_path = "/omd/sites/home/tmp/run/live"
 
-import socket
+#### SERVICES #####
 s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 s.connect(socket_path)
 
@@ -19,9 +20,26 @@ s.shutdown(socket.SHUT_WR)
 
 # Now read the answer
 answer = s.recv(100000000)
-
 # Parse the answer into a table (a list of lists)
 table = [ line.split(';') for line in answer.split('\n')[:-1] ]
+
+#### DOWNTIMES #####
+s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+s.connect(socket_path)
+# Write command to socket
+s.send("GET downtimes\n")
+s.send("Columns: host_name\n")
+
+# Important: Close sending direction. That way
+# the other side knows we are finished.
+s.shutdown(socket.SHUT_WR)
+
+# Now read the answer
+answer_downtimes = s.recv(100000000)
+downtime_hosts = [ line for line in answer_downtimes.split('\n')[:-1] ]
+
+# filter all entries for hosts in downtime
+table = [ service for service in table if not service[0] in downtime_hosts ]
 
 # change scientific notation to float
 print "<<<stalesvc>>>"
@@ -29,3 +47,4 @@ for i,list in enumerate(table):
   table[i][-1] = str(round(float(list[-1]), 2))
   table[i][-2] = str(round(float(list[-2]), 2))
   print ";".join(table[i])
+
